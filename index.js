442c5a81d4e7cd32cffdbad381dbf13534661ee4
Porter Libby <pelibby16@earlham.edu>
2023-12-16 20:05:38 -0500

    big update

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var favicon =           require('serve-favicon');
 const { QuickDB } =     require("quick.db");
 const bodyParser =      require('body-parser');
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+
 
 const db = new QuickDB();
 const app = express();
 const jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(urlencodedParser);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 
 app.get('/fetch', async function(req, res) {
     console.log(`attempt to fetch ${req.query.id}`)
@@ -25,7 +26,8 @@ app.post('/create', jsonParser, async function(req, res) {
         name: req.body.name,
         author: req.body.author,
         id: id,
-        text: req.body.text
+        text: req.body.text,
+        notes: req.body.notes
     };
     await db.set(id, newDeck);
     res.send(id);
@@ -33,8 +35,12 @@ app.post('/create', jsonParser, async function(req, res) {
 
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-    res.render("index",{
+    res.render("mtgpaste",{
     })
+});
+app.get('/syntax', function(req, res) {
+  res.render("syntax",{
+  })
 });
 
 app.use(express.static(__dirname + '/public'));
